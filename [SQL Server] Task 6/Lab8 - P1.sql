1d51efa22abec21aff840f2e7c02dda3c0ec15df
NTDuong96 <duongnguyenibd@gmail.com>
2022-06-24 08:52:05 +0700

    [SQL Server] Task 6
    Lab 8 (incompleted), question 1+3

---
+++ [SQL Server] Task 6/Lab8 - P1.sql
@@ -0,0 +1,16 @@
+create database Lab10
+go
+
+use AdventureWorks2019
+select * into Lab10.dbo.WorkOrder from Production.WorkOrder
+
+use Lab10
+select * into WorkOrderIX from WorkOrder
+
+select * from WorkOrder
+select * from WorkOrderIX
+
+create index IX_WorkOrderID on WorkOrderIX(WorkOrderID)
+
+select * from WorkOrder where WorkOrderID=72000
+select * from WorkOrderIX where WorkOrderID=72000
---
+++ [SQL Server] Task 6/Lab8 - P3.sql
@@ -0,0 +1,46 @@
+﻿create database Aptech
+go
+
+use Aptech
+go
+
+create table Classes(
+			ClassName char(6),
+			Teacher varchar(30),
+			TimeSlot varchar(30),
+			Class int,
+			Lab int
+)
+go
+
+--Tạo unique clustered index (MyClusteredIndex) {thuộc tính}
+create unique clustered index MyClusteredIndex
+	on Classes(ClassName) with
+	{
+	pad_index=on,
+	fillfactor=70,
+	ignore_dup_key=on
+	};
+go
+/* pad_index là công tắc để bật/tắt fill factor (fillfactor chỉ quy định số %)
+	ignore_dup_key chuyển on để từ chối việc nhập các giá trị trùng nhau/đã có trước vào bảng */
+--Tạo non clustered index (TeacherIndex)
+create nonclustered index TeacherIndex
+	on Classes(Teacher);
+go
+
+--Xóa (drop) TeacherIndex
+drop index TeacherIndex on Classes(Teacher)
+
+create unique clustered index MyClusteredIndex
+	on Classes(ClassName) with
+	{
+	drop_existing=on,
+	allow_row_locks=on,
+	allow_page_locks=on,
+	maxdop=2
+	};
+go
+--drop existing=on <=> drop index nếu đã trùng tên, index tạo mới cùng tên sẽ thay thuộc tính trong {}
+--allow row/page locks đặt on => index này cho phép lock các object (row/page) tương ứng
+-- ---- Lock
